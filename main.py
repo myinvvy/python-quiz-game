9d0e04692c1cc8c6ef5754a147e4995a20345990
myinvvy <yuthvictor@gmail.com>
2026-01-11 21:09:03 +1100

    Improvements
    Small changes to the quiz game
    Fixed typos within options
    Enumerate instead of manual iteration
    Invalid choices are handled
    Final score is rounded

--- main.py
+++ main.py
@@ -12,37 +12,41 @@ questions = {
 }
 
 options = [
-    ['A: cout << "Hello, world!"','B: console.log("Hello, world!");','C: echo "Hello, world!','D: print("Hello, world!")'],
+    ['A: cout << "Hello, world!"','B: console.log("Hello, world!");','C: echo "Hello, world!"','D: print("Hello, world!")'],
     ['A: variable_name = True','B: let variable_name = True','C: bool variable_name = True','D: local variable_name = True'],
     ['A: "Hello," + variable"','B: "Hello, variable"','C: "Hello, $variable"','D: f"Hello, {{variable}}"'],
     ['A: str_variable.upper()','B: str_variable.capitalize()','C: upper(str_variable)','D: capitalize(str_variable)'],
     ['A: function_name()','B: def function_name()','C: create function_name()','D: import function_name()'],
     ['A: () [] {}','B: [] () {}','C: {} [] ()','D: {} () []'],
     ['A: 2nd = "Silver Medal"','B: result = 1 + 1.0','C: answer = True or False','D: name = input("What is your name? ")'],
-    ['A: file.write()','B: os.remove()','C: shutil.rmtree()','os.rmdir()'],
+    ['A: file.write()','B: os.remove()','C: shutil.rmtree()','D: os.rmdir()'],
 ]
+
+valid_choices = ["A", "B", "C", "D"]
 
 def quiz():
     print("Welcome to the Python quiz.\n")
-    question_n = 0
     correct = 0
 
-    for i in questions:
-        print(i)
+    for question_number, question in enumerate(questions):
+        print(question)
 
-        for j in options[question_n]:
-            print(j)
+        for option in options[question_number]:
+            print(option)
 
-        choice = input("\nWhat is your choice? (A/B/C/D): ")
-        correct += check_answer(choice, i)
-        
+        while True:
+            choice = input("\nWhat is your choice? (A/B/C/D): ").upper()
+            if choice in valid_choices:
+                break
+            print("Please type A, B, C or D.")
+
+        correct += check_answer(choice, question)
         time.sleep(1)
-        question_n += 1
 
     quiz_end(correct)
 
-def check_answer(choice, i):
-    if choice == (questions.get(i)):
+def check_answer(choice, question):
+    if choice == (questions[question]):
         print("Your choice is correct!\n")
         return 1
     else:
@@ -50,11 +54,11 @@ def check_answer(choice, i):
         return 0
     
 def quiz_end(correct):
-    print(f"\nThank you for taking the quiz.\nYour final score is {(correct / len(questions) * 100)}%")
+    print(f"\nThank you for taking the quiz.\nYour final score is {round((correct / len(questions) * 100))}%")
 
 
 while True:
     quiz()
-    response = input('Would you like to retake the quiz? (Type "Yes" to continue): ').capitalize()
-    if response != "Yes":
+    response = input('Would you like to retake the quiz? (Type "Yes" to continue): ').strip().lower()
+    if response != "yes":
         break
